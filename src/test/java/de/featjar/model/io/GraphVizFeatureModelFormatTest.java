95c0d56c245ca1afcdb64a8f3e92381a2f22b4f4
Elias Kuiter <info@elias-kuiter.de>
2022-08-17 17:53:35 +0200

    URLs updated

--- src/test/java/de/featjar/model/io/GraphVizFeatureModelFormatTest.java
+++ src/test/java/de/featjar/model/io/GraphVizFeatureModelFormatTest.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with model. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatJAR/model> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-model> for further information.
  */
 package de.featjar.model.io;
 
